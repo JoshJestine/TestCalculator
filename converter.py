5d6a077b09ff29fae832298a027dd75832240068
Josh Jestine <joshjustin007@gmail.com>
2022-10-15 17:50:35 +0530

    1

--- converter.py
+++ converter.py
@@ -1,3 +1,5 @@
-from tkinter import *
+from simple_colors import *
 
-print "This is a converter for all units of measurement"
+print ("This is a converter for all units of measurement")
+print ('All-in-one conversion is supported')
+
