4f8808ba49088d7c446a72a26b8eab5795c2d28f
Josh Jestine <joshjustin007@gmail.com>
2022-10-17 00:31:20 +0530

    canvas

--- converter.py
+++ converter.py
@@ -5,3 +5,6 @@ from tkinter import Table
 print ('italic:', green('This is a converter for all units of measurement','italic'))
 print ('All-in-one conversion is supported')
 
+canvas = Tk()
+canvas.title("Universal Converter")
+
