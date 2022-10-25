86256e7db45c87bcbfc6e47f8c90ae4a0644c397
Josh Jestine <joshjustin007@gmail.com>
2022-10-25 20:40:19 +0530

    added clock code

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
-from simple_colors import *
 from tkinter import *
+'''from simple_colors import *
 # normal and colored text
 print('Normal:', blue('Welcome Finxters!'))
 # print italic and colored text
 print('italic: ', green('Welcome Finxter!', 'italic'))
 # print italic and underlined and colored text
-print('Italic and Underlined: ', red('Welcome Finxter!', ['italic', 'underlined']))
+print('Italic and Underlined: ', red('Welcome Finxter!', ['italic', 'underlined']))'''
+
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title('Clock')
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	lbl.config(text = string)
+	lbl.after(1000, time)
+
+lbl = Label(root, font = ('calibri', 40, 'bold'),
+			background = 'purple',
+			foreground = 'white')
+
+lbl.pack(anchor = 'center')
+time()
+
+mainloop()
