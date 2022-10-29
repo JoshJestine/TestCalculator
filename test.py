c68d815204fa9797d8021880e363363b99e6b598
Josh Jestine <joshjustin007@gmail.com>
2022-10-29 21:40:24 +0530

    added gif

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from tkinter import *
+'''from tkinter import *'''
 '''from simple_colors import *
 # normal and colored text
 print('Normal:', blue('Welcome Finxters!'))
@@ -7,7 +7,7 @@ print('italic: ', green('Welcome Finxter!', 'italic'))
 # print italic and underlined and colored text
 print('Italic and Underlined: ', red('Welcome Finxter!', ['italic', 'underlined']))'''
 
-from tkinter.ttk import *
+'''from tkinter.ttk import *
 
 from time import strftime
 
@@ -27,3 +27,45 @@ lbl.pack(anchor = 'center')
 time()
 
 mainloop()
+
+
+======================================================================='''
+
+import tkinter as tk
+from PIL import Image
+
+root = tk.Tk()
+file="gif1.gif"
+
+info = Image.open(file)
+
+frames = info.n_frames  # gives total number of frames that gif contains
+
+# creating list of PhotoImage objects for each frames
+im = [tk.PhotoImage(file=file,format=f"gif -index {i}") for i in range(frames)]
+
+count = 0
+anim = None
+def animation(count):
+    global anim
+    im2 = im[count]
+
+    gif_label.configure(image=im2)
+    count += 1
+    if count == frames:
+        count = 0
+    anim = root.after(50,lambda :animation(count))
+
+def stop_animation():
+    root.after_cancel(anim)
+
+gif_label = tk.Label(root,image="")
+gif_label.pack()
+
+start = tk.Button(root,text="start",command=lambda :animation(count))
+start.pack()
+
+stop = tk.Button(root,text="stop",command=stop_animation)
+stop.pack()
+
+root.mainloop()
